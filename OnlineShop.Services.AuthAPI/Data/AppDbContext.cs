0cd6e49f450c86842fb7cafb7b5dbeccf57eec10
Александр Мокринский <mokrinskiy@vector-best.ru>
2024-04-16 12:18:16 +0700

    AuuthAPI

--- OnlineShop.Services.AuthAPI/Data/AppDbContext.cs
+++ OnlineShop.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineShop.Services.AuthAPI.Models;
 
 namespace OnlineShop.Services.AuthAPI.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {            
         }
       
+        public DbSet<AppUser> AppUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
