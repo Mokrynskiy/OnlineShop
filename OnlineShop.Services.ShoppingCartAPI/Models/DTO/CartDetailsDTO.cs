a182c36b7782ca4b3141dac43ab0e9b6411e0fa1
Алексей Пастернак <Pasternak@vector-best.ru>
2024-04-17 09:12:58 +0700

    fix++

--- OnlineShop.Services.ShoppingCartAPI/Models/DTO/CartDetailsDTO.cs
+++ OnlineShop.Services.ShoppingCartAPI/Models/DTO/CartDetailsDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace OnlineShop.Services.ShoppingCartAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Services.ShoppingCartAPI.Models.DTO
 {
     public class CartDetailsDTO
     {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeaderDTO? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDTO? Product { get; set; }
+        public int Count { get; set; }
     }
 }
