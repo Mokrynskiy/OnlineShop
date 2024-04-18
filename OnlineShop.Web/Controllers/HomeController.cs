7a374a55fb37ebe0c6eda768e3374e948ce448ea
Алексей Пастернак <Pasternak@vector-best.ru>
2024-04-18 14:14:01 +0700

    shoppingcart

--- OnlineShop.Web/Controllers/HomeController.cs
+++ OnlineShop.Web/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
+using IdentityModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Web.Models;
+using OnlineShop.Web.Service.IService;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace OnlineShop.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IShoppingCartService _shoppingCartService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IShoppingCartService shoppingCartService)
         {
             _logger = logger;
+            _shoppingCartService = shoppingCartService;
         }
 
         public IActionResult Index()
@@ -27,6 +33,42 @@ namespace OnlineShop.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Authorize]
+        [HttpPost]
+        [ActionName("ProductionDetails")]
+        public async Task<IActionResult> ProductDetails(ProductDto productDto)
+        {
+            var cart = new CartDTO()
+            {
+                CartHeaderDTO = new CartHeaderDTO
+                {
+                    UserId = User.Claims.Where(x => x.Type == JwtClaimTypes.Subject)?.FirstOrDefault()?.Value
+                }
+            };
+
+            var cartDetails = new CartDetailsDTO()
+            {
+                Count = productDto.Count,
+                ProductId = productDto.Id
+            };
+
+            var cartDetailsDTOs = new List<CartDetailsDTO>() { cartDetails };
+            cart.CartDetails = cartDetailsDTOs;
+
+            var response = await _shoppingCartService.CartAddAsync(cart);
+
+            if(response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Товар успешно добавлен в корзину!";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(productDto);
         }
     }
 }
---
+++ OnlineShop.Web/Models/CartDetailsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Web.Models
+{
+    public class CartDetailsDTO
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeaderDTO? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
